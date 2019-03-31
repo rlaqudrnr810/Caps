e704a904e990342c75686f54b8a2c4150b994088
yeop <yeop@cau.ac.kr>
2019-03-31 17:04:03 +0900

    edit wrong file

--- django_test/mysite/elections/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Candidate,Poll
-# Register your models here.
-
-admin.site.register(Candidate)
-admin.site.register(Poll)
--- django_test/mysite/elections/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-app_name='elections'
-urlpatterns = [
-	url(r'^$', views.index,name='home'),
-	url(r'^areas/(?P<area>.+)/$', views.areas),
-]
